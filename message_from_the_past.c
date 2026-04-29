ccd3e250f1523d386c970db748bd8cf3e139cc92
Jack-of-all-trades-04 <novanagung08@gmail.com>
2026-04-29 09:21:48 +0700

    CS2Normal

---
+++ message_from_the_past.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("\033[1;34m");
+    printf("Written by Tekkom UI 2024\n");
+    printf("\033[0m");
+
+    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
+    printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+}
